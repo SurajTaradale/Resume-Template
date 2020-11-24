389f9d3212e19d6d999002b159cccec3104c578f
Suraj Taradale <surajtaradale@gmail.com>
2020-11-24 22:18:14 +0530

    Resume template using html css javascript

---
+++ script.js
@@ -0,0 +1,53 @@
+
+function resume_details1(){
+   
+    document.getElementById('heading').innerHTML ="Education qualification"
+    //graduation college name 
+    document.getElementById('content').innerHTML= "Graduation : Graduation college , 2020 with 10.00 CGPA."
+    // puc II college name
+    document.getElementById('content1').innerHTML= "PUC II : puc college name , 2016 with 90.5%."
+    // SSCL school name
+    document.getElementById('content2').innerHTML= "SSLC : SSLC school name, 2014 with 80.96%."
+    // if you want to add extra line then you write in " "(double codes)
+    document.getElementById('heading1').innerHTML =" "
+    document.getElementById('content3').innerHTML= " "
+    document.getElementById('content4').innerHTML= " "
+}
+function resume_details2(){
+   
+    document.getElementById('heading').innerHTML ="Skill set"
+    //can you write skills 
+    document.getElementById('content').innerHTML= "Programming Languages : C, Core java."
+    document.getElementById('content1').innerHTML= "Scripting Languages : HTML5, CSS, and JavaScript."
+    document.getElementById('content2').innerHTML= "RDBMS : SQL Server."
+    // if you want to add extra line then you write in " "(double codes)
+    document.getElementById('heading1').innerHTML =" "
+    document.getElementById('content3').innerHTML= " "
+    document.getElementById('content4').innerHTML= " "
+}
+function resume_details3(){
+   // can you mention here work expericence and projects
+    document.getElementById('heading').innerHTML =" Academic Major Project :"
+    document.getElementById('content').innerHTML= "=> School Management System"
+    document.getElementById('content1').innerHTML= "=> Employee Management System"
+    document.getElementById('content2').innerHTML= "=> Wind Energy Project"
+    document.getElementById('heading1').innerHTML =" Academic Minor Project :"
+    document.getElementById('content3').innerHTML= "=> Advertising Using location-based Service"
+    // if you want to add extra line then you write in " "(double codes)
+    document.getElementById('content4').innerHTML= " "
+}
+function resume_details4(){
+    document.getElementById('heading').innerHTML =" Personal Minutiae :"
+    //Full name in personal details
+    document.getElementById('content').innerHTML= "Name :abcd pqr xyz."
+    // date of birth
+    document.getElementById('content1').innerHTML= "Date of Birth : 1st  June 1998"
+    // languages 
+    document.getElementById('content2').innerHTML= "Language known : English, Hindi, Marathi, Kannada."
+    //please leave it black beacuse code will override 
+    document.getElementById('heading1').innerHTML =" "
+    // gender change blow
+    document.getElementById('content3').innerHTML =" Gender : Male (Single) "
+    // Nationality 
+    document.getElementById('content4').innerHTML= "Nationality : Indian ."
+}
